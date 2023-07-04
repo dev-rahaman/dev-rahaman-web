536ef48d7d86915bdcba9dd0b74b6425c2fec423
rs rahaman <jesminakter.rahaman@gmail.com>
2023-07-04 21:36:19 +0600

    complted for just now

---
+++ rs-rahaman-server/index.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+// middleware
+// app.use(cors());
+app.use(cors());
+app.use(express.json());
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri = `mongodb+srv://${process.env.MONGODB_USER_KEY}:${process.env.MONGODB_PASS_KEY}@cluster0.bk91ias.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    client.connect();
+
+    // await client.db("admin").command({ ping: -1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("zone is running");
+});
+
+// ----------------------------------COLLECTION----------------------------------
+const WorkSimpleCollection = client.db("rs-rahaman").collection("WorkSimple");
+
+app.get("/work-simple", async (req, res) => {
+  const result = await WorkSimpleCollection.find().sort({ _id: -1 }).toArray();
+  res.send(result);
+});
+
+// ----------------------------------ADMIN END----------------------------------
+
+// PAYMENT RELATED API
+
+// TODO: Make sure to sort the payment history descending. The newest payment will be at the top
+
+app.listen(port, () => {
+  console.log(`skillz zone is sitting on port ${port}`);
+});
