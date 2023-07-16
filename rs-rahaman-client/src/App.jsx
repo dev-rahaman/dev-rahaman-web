5bcb476075987da8f7702ecfcbee46670de6ecec
rs rahaman <jesminakter.rahaman@gmail.com>
2023-07-16 15:42:46 +0600

    dsfjsd

--- rs-rahaman-client/src/App.jsx
+++ rs-rahaman-client/src/App.jsx
@@ -1,12 +1,15 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+// import MessengerCustomerChat from "react-messenger-customer-chat";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return (
+    <>
+      {/* <MessengerCustomerChat
+        pageId="108532492303347"
+        appId="1146986992680900"
+      /> */}
+    </>
+  );
 }
 
 export default App;
